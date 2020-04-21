8f79d1281587c2edc2506c903786b87fc7825a8e
Zakir Hossen <web.zakirbd@gmail.com>
2020-04-21 19:54:25 +0600

    setting table seeder created

---
+++ database/seeds/SettingTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SettingTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        App\Setting::create([
+            'name' => 'Example.com',
+            'copyright' => 'Copyright © 2020 All rights reserved',
+        ]);
+    }
+}
